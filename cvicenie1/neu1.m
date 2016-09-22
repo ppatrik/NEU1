9498255153985800151f658e443582632a09699d
ppatrik <patrik.pekarcik@upjs.sk>
2016-09-22 11:46:45 +0200

    Update neu1.m

--- cvicenie1/neu1.m
+++ cvicenie1/neu1.m
@@ -1,5 +1,16 @@
 % prvy program
-xx=[3 4; 3 7; 8 7; 5 9; -1 -1; -2 5; -3 3; 2 5; 5 2; 4 2];
+%xx=[3 4; 3 7; 8 7; 5 9; -1 -1; -2 5; -3 3; 2 5; 5 2; 4 2];
+xx=[
+    3 4 -1; 
+    3 7 -1; 
+    8 7 -1; 
+    5 9 -1; 
+    -1 -1 -1; 
+    -2 5 -1; 
+    -3 3 -1; 
+    2 5 -1; 
+    5 2 -1; 
+    4 2 -1];
 d=[-1; 1; 1; 1; -1; -1; -1; -1; 1; -1];
 
 f1=figure; % figure ak idem vykreslovať
@@ -21,8 +32,21 @@ end;
 axis([-4 10 -3 10]) % nastavenie súradníc
 
 % priklad 3: hladanie priamky pre demonstraciu uciaceho pravidla
+w0=[1 2 3];
 x1=-4:1:10;
-x2=(3-x1)/2;
+x2=(w0(3)-w0(1)*x1)/w0(2);
 plot(x1, x2, '-m', 'LineWidth', 2) % vykreslenie ciary
 
-w0=[1 2 3];
+for i=1:5000
+    
+    for ii=1:size(xx, 1)
+        z=Neuron(w0, xx(1, :));
+        if z~=d(ii)
+            w0=w0+0.5*(d(ii)-z)*xx(ii,:);
+        end;
+    end;
+
+    x1=-4:1:10; % od -4 krok 1 do 10
+    x2=(w0(3)-w0(1)*x1)/w0(2);
+    plot(x1, x2, '-y', 'LineWidth', 1) % vykreslenie ciary
+end
