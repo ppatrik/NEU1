72488d97923c64c2909d2a1971bccd5f1f97d251
ppatrik <patrik.pekarcik@upjs.sk>
2016-10-06 11:12:04 +0200

    Update neuromat.m

--- cvicenie2/neuromat.m
+++ cvicenie2/neuromat.m
@@ -1,16 +1,17 @@
 clear % resetovanie premennych
 N=5; % počet klasických neurónov
-Typy=[1 0 0 1 0 -1 -1 -1];
+% Jazyk L = [(1(0+0(1+0)))*]
+Typy=[1 0 0 1 0 -1 -1 -1]; 
 Hrany=[
     1 2;
     2 1;
     1 3;
-    2 N+2;
     3 4;
     3 5;
     4 1;
-    4 N+2;
     5 1;
+    2 N+2;
+    4 N+2;
     5 N+2;
     N+3 N+2;
     N+3 1;
@@ -27,6 +28,8 @@ for k=1:size(Hrany)
     i=Hrany(k,2);
     ed(j,i)=1;
 end;
+
+Hrany
 
 % Vypocet d
 for i=1:N+3
@@ -73,16 +76,17 @@ z=Stav(N,4,1,ed,Stavy,Prahy,w);
 
 % dokoncit vypocet DU
 
-xx=[1 0 1 1 0 0]'; % skumane slovo
+xx=[1 0 1 1 0 0 1 1]'; % skumane slovo
 Stavy(N+1)=xx(1);
 for iii=1:size(xx)
     x=xx(iii);
     Stavyn(N+3)=Stav(N,N+3,x,ed,Stavy,Prahy,w);
     Stavyn(N+2)=Stav(N,N+2,x,ed,Stavy,Prahy,w);
+    Stavyn(N+1)=x;
     % tu pride vypocet novych stavov od i =1:N pocitam podla funkcie
     for iiii=1:N
         Stavyn(iiii)=Stav(N,iiii,x,ed,Stavy,Prahy,w);
     end
-    Stavyn
+    Stavyn(N+2) % vystupny neuron
     Stavy=Stavyn;
 end
